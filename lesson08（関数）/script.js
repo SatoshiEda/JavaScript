8041c258275471fb93f8037655f254aa86464ed6
衣田 敏 <satoshi_eda@icloud.com>
2024-04-06 00:07:37 +0900

    関数修正

--- lesson08（関数）/script.js
+++ lesson08（関数）/script.js
@@ -36,15 +36,3 @@ function calculateTotal02(price){
 
     return str;
 }
-
-/*
-Date.prototype.sayHello = function() {
-    return 'おはよう　今は ' + this.getFullYear() + '年です。';
-};
-const date01 = new Date();
-
-console.log(date01.sayHello());
-
-console.log(Date.prototype);
-
- */
